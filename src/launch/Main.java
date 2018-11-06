8c65bfb79c2e387e916c7c88a2914983dcf214de
Marcelo Moreira <36937456+MarceloMra@users.noreply.github.com>
2018-11-05 23:57:59 -0300

    Conceito inicial

--- src/launch/Main.java
+++ src/launch/Main.java
@@ -5,12 +5,45 @@
  */
 package launch;
 
+import java.util.Scanner;
+import model.*;
 /**
  *
  * @author Gigo
  */
 public class Main {
     public static void main(String[] args) {
+        int opc = 0, dimensao;
+        Scanner dado = new Scanner(System.in);
         
+        //Menu
+        do{
+            do{
+                System.out.println("-----> MENU");
+                System.out.println(" 1 - Criar arquivo de instância");
+                System.out.println(" 0 - Fechar Programa");
+                System.out.println("Digite a opção desejada: ");
+                opc = dado.nextInt();
+            }while(opc < 0 || opc > 1);
+            
+            switch(opc){
+                case 1:
+                    InstancesCreator criador = new InstancesCreator();
+                    
+                    do{
+                        System.out.println("Digite a dimensão do tabuleiro: ");
+                        dimensao = dado.nextInt();
+                    }while(dimensao < 1);
+                    
+                    System.out.println("... CRIANDO [instancia"+dimensao+"x"+dimensao+".txt] ...");
+                    
+                    if(criador.criarInstancia(dimensao)){
+                        System.out.println("Instância criada com sucesso!");
+                    }else{
+                        System.out.println("Não foi possivel criar a instância solicitada!");
+                    }
+                    break;
+            }
+        }while(opc != 0);
     }
 }
